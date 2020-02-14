183f8e42a3ed0d8661870de5b8acd775109fb66f
anil-kumar-lenka <mailmeanil97@gmail.com>
2020-02-14 21:14:36 +0530

    ReadKey() Added

--- 5 C# Assignments/Assignment 1 (Tax Calculation System)/TaxCalculationSystem/TaxCalculator.UI/Program.cs
+++ 5 C# Assignments/Assignment 1 (Tax Calculation System)/TaxCalculationSystem/TaxCalculator.UI/Program.cs
@@ -1,4 +1,5 @@
-﻿using TaxCalculator.Business;
+﻿using System;
+using TaxCalculator.Business;
 
 namespace TaxCalculator.UI
 {
@@ -18,6 +19,8 @@ namespace TaxCalculator.UI
             //for output to the user
             var output = new OutputTaxDetails();
             output.OutputTaxInfo(TaxDetails, input.salary);
+
+            Console.ReadKey();
         }
     }
 }
